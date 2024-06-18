385fc72cd9af399bcef0820d188533a668b40216
Ari Effendi <ari@arped.id>
2024-06-18 11:35:07 -0400

    First commit

---
+++ laravel10/idx-template.nix
@@ -0,0 +1,22 @@
+{pkgs}: {
+  packages = [
+    pkgs.php82
+    pkgs.php82Packages.composer
+  ];
+
+  bootstrap = ''
+    composer create-project laravel/laravel:10.x "$out"
+
+    cd "$out"
+
+    composer install
+
+    cp .env.example .env
+
+    php artisan key:generate
+
+    mkdir -p "$out/.idx/"
+
+    cp -rf ${./dev.nix} "$out/.idx/dev.nix"
+  ''
+}
