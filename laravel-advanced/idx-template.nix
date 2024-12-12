18beffe6d2deb5229ee4f74b5ea21fa58914c9dc
Ari Effendi <ari@arped.id>
2024-12-12 22:56:33 +0700

    Set php version using sed

--- laravel-advanced/idx-template.nix
+++ laravel-advanced/idx-template.nix
@@ -19,17 +19,11 @@
 
     mkdir -p "$out/.idx/"
 
-  if [ "${php}" == "php83" ]; then \
-    cp -rf $(./php83.nix} "$out/.idx/dev.nix"; \
-    elif [ "${php}" == "php84" ]; then \
-    cp -rf $(./php84.nix} "$out/.idx/dev.nix"; \
-    else \
-    cp -rf $(./php82.nix} "$out/.idx/dev.nix"; \
-    fi
+    cp -rf ${./dev.nix} "$out/.idx/dev.nix"
+
+    sed -i "s/php82/${php}/g" "$out/.idx/dev.nix"
 
     # Fix carbon error.
-    if [ "${version}" == "12.x-dev" ]; then \
-      sed -i "s/'lifetime' => env('SESSION_LIFETIME', \([0-9]*\))/'lifetime' => (int) env('SESSION_LIFETIME', \1)/" "$out/config/session.php"; \
-      fi
+    sed -i "s/'lifetime' => env('SESSION_LIFETIME', \([0-9]*\))/'lifetime' => (int) env('SESSION_LIFETIME', \1)/" "$out/config/session.php"
   '';
 }
