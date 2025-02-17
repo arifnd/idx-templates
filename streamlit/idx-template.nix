27fbb5c43d6e004c5aea9825734896e682612bce
Ari Effendi <ari@arped.id>
2025-02-18 01:25:11 +0700

    Fix bootstrap

--- streamlit/idx-template.nix
+++ streamlit/idx-template.nix
@@ -1,18 +1,7 @@
 {pkgs}: {
-  packages = [
-    pkgs.python312
-    pkgs.python312Packages.pip
-  ];
-
   bootstrap = ''
     export HOME=/home/user
 
     cp -af ${./files/.} "$out"
-
-    cd "$out"
-
-    python -m env stenv
-
-    "$out/stenv/bin/python -m pip install -U streamlite"
   '';
 }
