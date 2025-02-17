3f50e904a84d73ff783cb0b450c1c69ece814649
Ari Effendi <ari@arped.id>
2025-02-18 01:35:17 +0700

    Fix folder permission

--- streamlit/idx-template.nix
+++ streamlit/idx-template.nix
@@ -1,7 +1,20 @@
 {pkgs}: {
+  packages = [
+    pkgs.python312
+    pkgs.python312Packages.pip
+  ];
+
   bootstrap = ''
     export HOME=/home/user
 
     cp -af ${./files/.} "$out"
+
+    chmod -R 755 "$out"
+
+    cd "$out"
+
+    python -m env stenv
+
+    "$out/stenv/bin/python -m pip install -U streamlit"
   '';
 }
